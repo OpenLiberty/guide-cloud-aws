e4274c598dcb8de6516a68977c17b40793fb79ab
Austin Seto <austinseto@gmail.com>
2021-01-08 10:52:07 -0500

    Daily builds (#98)
    * Update for daily builds
    
    * Rename step
    
    * Add testing of daily docker image
    
    * Update PR tests
    
    * Make files executable
    
    * Update for testing OL docker image
    
    * Update test.yml
    
    * Update test.yml
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: Austin Seto <austin.seto@ibm.com>

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+while getopts t:d:b:u: flag;
+do
+    case "${flag}" in
+        t) DATE="${OPTARG}";;
+        d) DRIVER="${OPTARG}";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}";;
+    esac
+done
+
+echo "Testing daily OpenLiberty image"
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" inventory/pom.xml system/pom.xml
+cat inventory/pom.xml system/pom.xml
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" system/Dockerfile inventory/Dockerfile
+cat system/Dockerfile inventory/Dockerfile
+
+docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+
+../scripts/startMinikube.sh
+../scripts/testApp.sh
+../scripts/stopMinikube.sh
