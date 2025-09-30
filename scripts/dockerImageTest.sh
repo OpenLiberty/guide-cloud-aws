e889539f7c4a9a4ce7ff1529fea0d2c3085ed651
Gilbert Kwan <gkwan@ca.ibm.com>
2025-09-30 11:29:16 -0400

    Change to use image from cp.stg.icr.io/cp/olc

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -15,15 +15,15 @@ sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install
 cat inventory/pom.xml system/pom.xml
 
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile
 else
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile
 fi
 sed -i "s;RUN features.sh;;g" system/Dockerfile inventory/Dockerfile
 cat system/Dockerfile inventory/Dockerfile
 
 echo "$DOCKER_PASSWORD" | sudo -u runner docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
-sudo -u runner docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi"
-sudo -u runner echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi
+sudo -u runner docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
+sudo -u runner echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi
 
 sudo -u runner ../scripts/testApp.sh
